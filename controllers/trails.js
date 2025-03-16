66467bb83d6c0897b0e5b295c7485f9bbd3327e6
meltphace7 <brd9326@protonmail.com>
2025-03-16 10:25:09 -0700

    added api key to env

--- controllers/trails.js
+++ controllers/trails.js
@@ -16,8 +16,9 @@ const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-const openWeatherKey = process.env.OPENWEATHER_KEY;
-const openWeatherKey2 = process.env.OPENWEATHER_KEY_2;
+const pirateWeatherKey = process.env.PIRATEWEATHER_KEY;
+// const openWeatherKey = process.env.OPENWEATHER_KEY;
+// const openWeatherKey2 = process.env.OPENWEATHER_KEY_2;
 
 // CONFIGURES s3 object so the image can be stored
 const s3 = new S3Client({
@@ -298,5 +299,5 @@ exports.postDeleteTrail = (req, res, next) => {
 };
 
 exports.getTrailWeatherKey = (req, res, next) => {
-  res.status(201).json({ openWeatherKey: openWeatherKey });
+  res.status(201).json({ pirateWeatherKey: pirateWeatherKey });
 };
